fecfd74e113ce3aa023af4d35f9e17c23057bfe1
nowanjiru <mynahnoreen@gmail.com>
2026-07-29 15:53:04 +0300

    feat(users): implement user model

--- backend/internal/users/model.go
+++ backend/internal/users/model.go
@@ -1,1 +1,12 @@
 package users
+
+import "time"
+
+type User struct {
+	ID           int       `json:"id"`
+	Username     string    `json:"username"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"password_hash"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
