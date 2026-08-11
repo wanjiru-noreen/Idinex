fafe9aeae5b87bc072b1030bb15e7dd4f70e7256
mmicbee <ann608888@gmail.com>
2026-08-11 14:32:10 +0300

    feat(auth):find user model

--- backend/internal/auth/model.go
+++ backend/internal/auth/model.go
@@ -1,5 +1,16 @@
-/*
-User model
-RefreshToken model
-*/
 package auth
+
+import ( 
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID  uuid.UUID 'json:"id"'
+	Username string 'json:"username"'
+	Email  string 'json:"email"'
+	PassWordHash string 'json:"-"'
+	CreatedAt  time.Time 'json:"created_at"'
+	UpdatedAt time.Time 'json:"updated_at"'
+}
