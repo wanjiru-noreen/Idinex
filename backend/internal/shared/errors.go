162a9e80f0d2502b77a2f887834916d7d2bfa370
nowanjiru <mynahnoreen@gmail.com>
2026-08-17 12:37:41 +0300

    feat(shared): define authentication errors

--- backend/internal/shared/errors.go
+++ backend/internal/shared/errors.go
@@ -1,2 +1,10 @@
-// custom errors
 package shared
+
+import "errors"
+
+var (
+	ErrInvalidCredentials = errors.New("invalid_credentials")
+	ErrInvalidRequest     = errors.New("invalid_request")
+	ErrEmailAlreadyExists = errors.New("email_already_exists")
+	ErrInternalServer     = errors.New("internal_server_error")
+)
